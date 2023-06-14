81a36385198130b14df807e9d8ccf0cea4e50308
Merlin <merlin.vop@gmail.com>
2023-06-14 23:13:07 +0300

    Крок 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,14 +5,16 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   handleSubmit = evt => {
     evt.preventDefault();
-    const { name } = evt.currentTarget.elements;
+    const { name, number } = evt.currentTarget.elements;
     const contactItem = {
       id: nanoid(),
       name: name.value,
+      number: number.value,
     };
     console.log(this.state);
     console.log(contactItem);
@@ -23,7 +25,9 @@ export class App extends Component {
   };
 
   listContacts = () => {
-    const list = this.state.contacts.map(contact => contact.name + ' ');
+    const list = this.state.contacts.map(
+      contact => '*' + contact.name + ' ' + contact.number + '\r'
+    );
     return list;
   };
 
@@ -39,6 +43,14 @@ export class App extends Component {
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+            <p>Number</p>
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
             />
             <p>
